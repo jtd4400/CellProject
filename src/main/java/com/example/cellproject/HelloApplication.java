df15ad187643e14856255ab1c1cc778ca7aab523
jtd4400 <jtd4400@rit.edu>
2024-12-07 15:44:54 -0500

    updates to UI

--- src/main/java/com/example/cellproject/HelloApplication.java
+++ src/main/java/com/example/cellproject/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.example.cellproject;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -44,6 +45,8 @@ public class HelloApplication extends Application {
         for (int r = 0; r < gridWidth; r++) {
             for (int c = 0; c < gridHeight; c++) {
                 Label l = cm.ParseStringToLabel(r, c);
+                l.setMinSize(30,30);
+                l.setMaxSize(30,30);
                 gp.add(l, c, r);
                 grid[c][r] = l;
             }
@@ -55,57 +58,58 @@ public class HelloApplication extends Application {
         rc = new Label("Red Cells: " + cm.getRedCount());
         rc.setStyle( """
                             -fx-text-fill: white;
+                            -fx-font-size: 40;
                 """);
         bl = new Label("Blue Cells: " + cm.getBlueCount());
         bl.setStyle( """
                             -fx-text-fill: white;
+                            -fx-font-size: 40;
                 """);
         green = new Label("Green Cells: " + cm.getGreenCount());
         green.setStyle( """
                             -fx-text-fill: white;
+                            -fx-font-size: 40;
                 """);
         dis_info.add(rc, 0, 0);
         dis_info.add(bl, 0, 1);
         dis_info.add(green, 0, 2);
         BorderPane left1 = new BorderPane();
-        left1.setMinWidth(200);
+        left1.setMinWidth(400);
         left1.setMinHeight(400);
-        left1.setMaxHeight(400);
+        left1.setMaxWidth(400);
         left1.setBackground(Background.fill(Color.BLACK));
         GridPane leftButtons = new GridPane();
         leftButtons.setVgap(10);
-        Button b1 = new Button("Randomly Kill Half");
-        b1.setOnAction(event -> killHalf());
 
         Label choiceLabel = new Label("Select BG Color:");
+        choiceLabel.setStyle( """
+                            -fx-text-fill: white;
+                            -fx-font-size: 40;
+                """);
         ChoiceBox<String> b2 = new ChoiceBox<>();
-        leftButtons.add(b1,0,0);
-        leftButtons.add(choiceLabel,0,1);
-        leftButtons.add(b2,0,2);
+        b2.setMinSize(100, 50);
+        leftButtons.add(choiceLabel,0,0);
+        leftButtons.add(b2,0,1);
         GridPane left2 = new GridPane();
-        left2.setVgap(10);
-        Label generation = new Label("Generation: ");
-        starter = new Button("Start");
-        AtomicInteger dependent = new AtomicInteger();
-        ender = new Button("Stop");
-        ender.setOnAction(event -> {
-            dependent.addAndGet(1);
-        });
-
+        Button b1 = new Button("Randomly Kill Half");
+        b1.setMinSize(400,50);
+        b1.setStyle("""
+                            -fx-text-fill: black;
+                            -fx-font-size: 40;
+                """);
+        b1.setOnAction(event -> killHalf());
+        starter = new Button("Advance");
         starter.setOnAction(event -> {
             cm.AdvanceGeneration();
-//            while (dependent.get() < 1) {
-//                cm.AdvanceGeneration();
-//                try {
-//                    Thread.sleep(1500);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
         });
-        left2.add(generation, 0, 0);
+        starter.setStyle("""
+                            -fx-text-fill: black;
+                            -fx-font-size: 40;
+                """);
+        starter.setMinSize(400,50);
+        left2.setHgap(5);
+        left2.add(b1,0,0);
         left2.add(starter, 0, 1);
-        left2.add(ender, 0, 2);
         left1.setTop(dis_info);
         left1.setCenter(leftButtons);
         left1.setBottom(left2);
@@ -124,6 +128,8 @@ public class HelloApplication extends Application {
         for (int r = 0; r < gridWidth; r++) {
             for (int c = 0; c < gridHeight; c++) {
                 Label l = cm.ParseStringToLabel(r, c);
+                l.setMinSize(30,30);
+                l.setMaxSize(30,30);
                 gp.add(l, c, r);
                 grid[c][r] = l;
             }
